c54f102079247f235d2e4dbaf1196d18414c403d
iliyanpaskov <108199866+iliyanpaskov@users.noreply.github.com>
2023-03-31 14:59:03 +0300

    Confirm Modal added to Comment component

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,27 +1,43 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import { useDispatch } from "react-redux";
-import {  fetchDeleteComment} from "../../redux/features/commentsSlice";
+import { fetchDeleteComment } from "../../redux/features/commentsSlice";
 import "./Comment.css";
+import Modal from "../Modal/Modal";
 
 const Comment = ({ comment }) => {
     const { user } = useContext(AuthContext);
+    const [openModal, setOpenModal] = useState(false);
     const isAuthor = comment.username === user.username;
     const dispatch = useDispatch();
+    const message = "delete your comment";
+
+    const openModalHandler = () => {
+        setOpenModal(true);
+    };
 
     const deleteHandler = () => {
         dispatch(fetchDeleteComment(comment.objectId));
-    }
+    };
+
     return (
-        <article className="comment-wrapper">
-            <p className="comment-content">{comment.comment}</p>
-            <p className="comment-author">{comment.username}</p>
-            {
-                isAuthor
-                    ? <i className="fa-sharp fa-regular fa-circle-xmark delete-comment" onClick={deleteHandler}></i>
-                    : null
-            }
-        </article>
+        <>
+            <article className="comment-wrapper">
+                <p className="comment-content">{comment.comment}</p>
+                <p className="comment-author">{comment.username}</p>
+                {
+                    isAuthor
+                        ? <i className="fa-sharp fa-regular fa-circle-xmark delete-comment" onClick={openModalHandler}></i>
+                        : null
+                }
+            </article>
+            <Modal
+                open={openModal}
+                onClose={() => setOpenModal(false)}
+                onDelete={() => deleteHandler()}
+                message={message}
+            />
+        </>
     )
 }
 export default Comment;
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,7 +1,6 @@
-import Logo from "../common/Logo/Logo";
 import "./Modal.css";
 
-const Modal = ({ open, onClose, onDelete }) => {
+const Modal = ({ open, onClose, onDelete, message }) => {
 
     if (!open) {
         return null;
@@ -10,8 +9,8 @@ const Modal = ({ open, onClose, onDelete }) => {
     return (
         <section className="modal-wrapper">
             <article className="modal">
-                <Logo />
-                <p className="modal-content">Are you sure you want to delete your comment?</p>
+                <i className="fa-sharp fa-solid fa-x" onClick={onClose}></i>
+                <p className="modal-content">Are you sure you want to <span className="modal-content-message">{message}</span>?</p>
                 <article className="modal-btns-wrapper">
                     <button className="modal-btn confirm-btn" onClick={onDelete}>yes</button>
                     <button className="modal-btn reject-btn" onClick={onClose}>no</button>
